b4c02eb35993ab869970a0d9afbfe342435e656c
Stuart Hoffman <stu@hoffmanswebsite.com>
2016-02-20 11:14:51 -0800

    Wired up 5 to the numberPressed action added output label support that is not quite working yet

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -10,14 +10,60 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    enum Operation: String{
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
     //Properties
     var btnSound: AVAudioPlayer!
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
     //Outlets
     @IBOutlet weak var outputLbl: UILabel!
     //Actions
     @IBAction func numberPressed(btn: UIButton)
     {
+        playButtonSound()
+        runningNumber += "\(btn.tag)"
+    }
+    
+    
+    @IBAction func onDividePressed(sender: AnyObject) {
+        playButtonSound()
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        playButtonSound()
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        playButtonSound()
+    }
+    
+    @IBAction func onAddPressed(sender: AnyObject) {
+        playButtonSound()
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        playButtonSound()
+    }
+    
+    func playButtonSound()
+    {
         btnSound.play()
+    }
+    
+    func updateOutputLabel()
+    {
+        outputLbl.text = "\(runningNumber)"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
